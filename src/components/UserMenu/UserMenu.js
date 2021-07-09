99e4687f2814ae300bf69b7088d37c2ff8ba5c81
Michal Lenyuk <mlcorp@ukr.net>
2021-07-09 09:35:35 +0300

    HM/9/v/1.2

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,78 +1,40 @@
-import React from 'react';
-import { connect } from "react-redux";
-import { authSelectors, authOperations } from "../../redux/auth";
-import defaultAvatar from "./default-avatar.jpg"
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { authSelectors, authOperations } from '../../redux/auth';
+import defaultAvatar from './default-avatar.jpg';
 
 const styles = {
     container: {
-        display: "flex",
-        alignItems: "center",
+        display: 'flex',
+        alignItems: 'center',
     },
     avatar: {
-        marginRight: 6,
+        marginRight: 4,
     },
     name: {
-        fontWeight: 800,
+        fontWeight: 700,
         marginRight: 12,
     },
 };
 
-const UserMenu = ({ avatar, name, onLogout }) => (
+export default function UserMenu() {
+  const dispatch = useDispatch();
+  const name = useSelector(authSelectors.getUserName);
+  console.log(
+    `useSelector(state => state)`,
+    useSelector((state) => state)
+  );
+  const onLogOut = useCallback(() => {
+    dispatch(authOperations.logOut());
+  }, [dispatch]);
+
+  return (
     <div style={styles.container}>
-        <img src={avatar} alt="" width="32" style={styles.avatar} />
-        <span style={styles.name}> Welcome, {name}</span>
-        <button type="button" onClick={onLogout}>
-            Logout
-        </button>
-    </div>    
-)
-
-const mapStateToProps = (state) => ({
-  name: authSelectors.getUserName(state),
-  avatar: defaultAvatar,
-});
-
-const mapDispatchToProps = {
-  onLogout: authOperations.logOut,
-};
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(UserMenu);
-
-// import React, { useCallback } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { authSelectors, authOperations } from '../../redux/auth';
-// import defaultAvatar from './default-avatar.png';
-
-// const styles = {
-//     container: {
-//         display: 'flex',
-//         alignItems: 'center',
-//     },
-//     avatar: {
-//         marginRight: 4,
-//     },
-//     name: {
-//         fontWeight: 700,
-//         marginRight: 12,
-//     },
-// };
-
-// export default function UserMenu() {
-//     const dispatch = useDispatch();
-//     const name = useSelector(authSelectors.getUsername);
-
-//     const onLogOut = useCallback(() => {
-//         dispatch(authOperations.logOut());
-//     }, [dispatch]);
-
-//     return (
-//         <div style={styles.container}>
-//             <img src={defaultAvatar} alt="" width="32" style={styles.avatar} />
-//             <span style={styles.name}>Welcome, {name}</span>
-//             <button type="button" onClick={onLogOut}>
-//                 Logout
-//             </button>
-//         </div>
-//     );
-// }
+      <img src={defaultAvatar} alt="" width="32" style={styles.avatar} />
+      <span style={styles.name}>Welcome, {name}</span>
+      <button type="button" onClick={onLogOut}>
+        Logout
+      </button>
+    </div>
+  );
+}
